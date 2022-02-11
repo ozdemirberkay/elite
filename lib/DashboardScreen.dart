e6e385b99816a37dcf76321bc20097e1a6a7f1f7
ozdemirberkay <ozdemirberkay1999@gmail.com>
2022-02-11 21:43:46 +0300

    düzenleme

--- lib/DashboardScreen.dart
+++ lib/DashboardScreen.dart
@@ -8,11 +8,38 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  showPage() {
+    if (_selectedIndex == 0) {
+      return Container(
+        child: Text("Anasayfa"),
+      );
+    } else if (_selectedIndex == 1) {
+      return Container(child: Text("Ayarlar"));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text("ELITE"),
+        centerTitle: true,
+      ),
+      body: showPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Anasayfa"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Ayarlar"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
