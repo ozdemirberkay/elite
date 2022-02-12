a5ec4ba9fc8467918bbe94fe6765d84eb1a68345
Han Demirer <43323219+handemirer@users.noreply.github.com>
2022-02-12 16:12:19 +0200

    category model update

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -2,10 +2,15 @@ import 'package:elite/model/article.dart';
 
 class Category {
   String id;
+  String? categoryImgUrl;
+  String title;
+
   List<Article> articles;
 
   Category({
     required this.id,
+    this.categoryImgUrl,
+    required this.title,
     required this.articles,
   });
 }
