9b59cba50483ede5998bebc3ef89b273cfd241a7
Han Demirer <43323219+handemirer@users.noreply.github.com>
2022-02-12 18:05:00 +0200

    header

--- lib/Screens/kategoriler.dart
+++ lib/Screens/kategoriler.dart
@@ -95,92 +95,112 @@ class _CategoriesState extends State<Categories> {
 
     return SafeArea(
       child: SingleChildScrollView(
-          child: StreamBuilder<QuerySnapshot>(
-        stream: categories,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: text(
+              "KATEGORİLER",
+              fontSize: textSizeLarge,
+              isCentered: true,
+              fontFamily: fontMedium,
+              textColor: appStore.isDarkModeOn ? white : quiz_textColorPrimary,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: StreamBuilder<QuerySnapshot>(
+              stream: categories,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading");
-          }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("Loading");
+                }
 
-          var category = snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            NewQuizModel newQuizModel = NewQuizModel();
-            newQuizModel.quizImage = data["categoryImgUrl"].toString();
-            newQuizModel.quizName = data["title"].toString();
-            newQuizModel.totalQuiz = "10 Makale";
-            mListings.add(newQuizModel);
-          }).toList();
+                var category =
+                    snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+                  NewQuizModel newQuizModel = NewQuizModel();
+                  newQuizModel.quizImage = data["categoryImgUrl"].toString();
+                  newQuizModel.quizName = data["title"].toString();
+                  newQuizModel.totalQuiz = "10 Makale";
+                  mListings.add(newQuizModel);
+                }).toList();
 
-          return StaggeredGridView.countBuilder(
-            crossAxisCount: 4,
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-            staggeredTileBuilder: (index) => StaggeredTile.fit(2),
-            scrollDirection: Axis.vertical,
-            itemCount: mListings.length,
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(16.0),
-                          topRight: Radius.circular(16.0)),
-                      child: CachedNetworkImage(
-                        placeholder: placeholderWidgetFn() as Widget Function(
-                            BuildContext, String)?,
-                        imageUrl: mListings[index].quizImage,
-                        height: context.width() * 0.4,
-                        width: MediaQuery.of(context).size.width / 0.25,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(16.0),
-                            bottomRight: Radius.circular(16.0)),
-                        color: context.cardColor,
-                      ),
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+                  scrollDirection: Axis.vertical,
+                  itemCount: mListings.length,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(8),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          text(mListings[index].quizName,
-                                  fontSize: textSizeMedium,
-                                  maxLine: 2,
-                                  fontFamily: fontMedium)
-                              .paddingOnly(
-                                  top: 8, left: 16, right: 16, bottom: 8),
-                          text(mListings[index].totalQuiz,
-                                  textColor: quiz_textColorSecondary)
-                              .paddingOnly(left: 16, right: 16, bottom: 16),
-                          LinearProgressIndicator(
-                            value: 0.5,
-                            backgroundColor:
-                                textSecondaryColor.withOpacity(0.2),
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(quiz_green),
-                          ).paddingOnly(left: 16, right: 16, bottom: 16),
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(16.0),
+                                topRight: Radius.circular(16.0)),
+                            child: CachedNetworkImage(
+                              placeholder: placeholderWidgetFn() as Widget
+                                  Function(BuildContext, String)?,
+                              imageUrl: mListings[index].quizImage,
+                              height: context.width() * 0.4,
+                              width: MediaQuery.of(context).size.width / 0.25,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(16.0),
+                                  bottomRight: Radius.circular(16.0)),
+                              color: context.cardColor,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                text(mListings[index].quizName,
+                                        fontSize: textSizeMedium,
+                                        maxLine: 2,
+                                        fontFamily: fontMedium)
+                                    .paddingOnly(
+                                        top: 8, left: 16, right: 16, bottom: 8),
+                                text(mListings[index].totalQuiz,
+                                        textColor: quiz_textColorSecondary)
+                                    .paddingOnly(
+                                        left: 16, right: 16, bottom: 16),
+                                LinearProgressIndicator(
+                                  value: 0.5,
+                                  backgroundColor:
+                                      textSecondaryColor.withOpacity(0.2),
+                                  valueColor:
+                                      AlwaysStoppedAnimation<Color>(quiz_green),
+                                ).paddingOnly(left: 16, right: 16, bottom: 16),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ).cornerRadiusWithClipRRect(16).onTap(() {
-                QuizDetails().launch(context);
-              });
-            },
-          );
-        },
+                    ).cornerRadiusWithClipRRect(16).onTap(() {
+                      QuizDetails().launch(context);
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       )),
     );
   }
